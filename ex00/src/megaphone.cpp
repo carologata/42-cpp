737ed0c10bda8060f5a46e7fd4ee3a0a7806fbab
cogata <carol.ogata1@gmail.com>
2024-07-25 11:18:54 -0300

    ex00

---
+++ ex00/src/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    std::string str;
+
+    if(argc > 1)
+    {
+        for(int i = 1; i < argc; i++)
+        {   
+            str = argv[i];
+            for(size_t j = 0; j < str.length(); j++)
+                std::cout << (char)std::toupper(str[j]);
+        }
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+}
